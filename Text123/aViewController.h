c600aabd07c002d46f47c3f65bc72b1f45549579
李杨 <279082641@qq.com>
2013-02-18 11:11:13 +0800

    add aViewController

---
+++ Text123/aViewController.h
@@ -0,0 +1,13 @@
+//
+//  aViewController.h
+//  Text123
+//
+//  Created by MrsLi on 13-2-18.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface aViewController : UIViewController
+
+@end
---
+++ Text123/aViewController.m
@@ -0,0 +1,38 @@
+//
+//  aViewController.m
+//  Text123
+//
+//  Created by MrsLi on 13-2-18.
+//  Copyright (c) 2013年 Ibokan. All rights reserved.
+//
+
+#import "aViewController.h"
+
+@interface aViewController ()
+
+@end
+
+@implementation aViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
